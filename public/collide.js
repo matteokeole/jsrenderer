cd1b30529b0c7efdb87d7775311dd58ae5b0df07
matteokeole <matteoo34@icloud.com>
2022-07-05 17:25:02 +0200

    GUI fresh start

--- public/collide.js
+++
@@ -1,5 +0,0 @@
-import {player, wall} from "./main.js";
-
-export default () => {
-	if (player.position.z + player.geometry.width / 2 >= wall.position.z) console.log("colliding")
-};
--- src/class/Texture.js
+++
@@ -1,7 +0,0 @@
-export function Texture(src) {
-	this.loadState = 0;
-	this.src = src;
-	this.image = new Image();
-
-	return this;
-};
